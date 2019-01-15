62dacf9aa219457dca6fc1a9a5c50c6881d6782a
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-15 21:10:13 +0100

    Finalizado punto 4 y pasado el test PermanenciaTest

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/Permanencia.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/Permanencia.java
@@ -2,47 +2,63 @@ package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Permanencia {
-	
+
 	private LocalDate dia;
-	private static final DateTimeFormatter FORMATO_DIA;
+	private static final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/uuuu");
 	private Tramo tramo;
-	
+
 	public Permanencia(LocalDate dia, Tramo tramo) {
-		
-	}
-	
-	public Permanencia(Permanencia p) {
-		
-	}
-	
-	public LocalDate getDia() {
-		
-	}
-	
-	private void setDia(LocalDate dia) {
-		
-	}
-	
-	public Tramo getTramo() {
-		
-	}
-	
-	private void setTramo(Tramo tramo) {
-		
-	}
-	
-	public int hashCode() {
-		
-	}
-	
-	public boolean equals(Object o) {
-		
-	}
-	
-	public String toString() {
-		
+		setDia(dia);
+		setTramo(tramo);
 	}
 
+	public Permanencia(Permanencia p) {
+		if(p==null)
+			throw new IllegalArgumentException("No se puede copiar una permanencia nula.");
+		setDia(p.getDia());
+		setTramo(p.getTramo());
+	}
+
+	public LocalDate getDia() {
+		return LocalDate.of(dia.getYear(), dia.getMonth(), dia.getDayOfMonth());
+	}
+
+	
+	private void setDia(LocalDate dia) {
+		if(dia==null)
+			throw new IllegalArgumentException("El día de una permanencia no puede ser nulo.");
+		this.dia = LocalDate.of(dia.getYear(), dia.getMonth(), dia.getDayOfMonth());
+	}
+
+	public Tramo getTramo() {
+		return tramo;
+	}
+
+	private void setTramo(Tramo tramo) {
+		if(tramo == null)
+			throw new IllegalArgumentException("El tramo de una permanencia no puede ser nulo.");
+		this.tramo = tramo;
+	}
+
+	public int hashCode() {
+		return Objects.hash(dia, tramo);
+	}
+
+	public boolean equals(Object o) {
+		if(o==null)
+			return false;
+		if(!(o instanceof Permanencia))
+			return false;
+		Permanencia otra = (Permanencia) o;
+		if(this.getDia().equals(otra.getDia()) && this.getTramo().equals(otra.getTramo()))
+			return true;
+		return false;
+	}
+
+	public String toString() {
+		return "[dia=" + getDia().format(FORMATO_DIA) + ", tramo=" + getTramo() + "]";
+	}
 }
