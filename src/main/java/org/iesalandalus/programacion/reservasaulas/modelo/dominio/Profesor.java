6e81780096fe4a1b2cda7b24118d19c23a2293fe
juanmanzanoplaza <45107012+juanmanzanoplaza@users.noreply.github.com>
2019-01-16 13:18:35 +0100

    Realizado punto 6 y pasado el test ProfesorTest

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/Profesor.java
@@ -0,0 +1,110 @@
+package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Profesor {
+
+	private static final String ER_TELEFONO = "^[69][0-9]{8}$";
+	private static final String ER_CORREO = "^[a-z0-9]+(\\.[a-z0-9]+)*@[a-z]+(\\.[a-z]+)*(\\.[a-z]{1,4})$";
+	private String nombre;
+	private String correo;
+	private String telefono;
+
+	public Profesor(String nombre, String correo) {
+		setNombre(nombre);
+		setCorreo(correo);
+	}
+
+	public Profesor(String nombre, String correo, String telefono) {
+		setNombre(nombre);
+		setCorreo(correo);
+		setTelefono(telefono);
+	}
+
+	public Profesor(Profesor otro) {
+		if(otro==null)
+			throw new IllegalArgumentException("No se puede copiar un profesor nulo.");
+		setNombre(otro.getNombre());
+		setCorreo(otro.getCorreo());
+		setTelefono(otro.getTelefono());
+			
+	}
+
+	private void setNombre(String nombre) {
+		if(nombre==null)
+			throw new IllegalArgumentException("El nombre del profesor no puede ser nulo.");
+		if(nombre.equals(""))
+			throw new IllegalArgumentException("El nombre del profesor no puede estar vacío.");
+		this.nombre = nombre;
+
+	}
+
+	public void setCorreo(String correo) {
+		if(correo==null)
+			throw new IllegalArgumentException("El correo del profesor no puede ser nulo.");
+		Pattern p = Pattern.compile(ER_CORREO);
+		Matcher m = p.matcher(correo);
+		if(m.matches())
+			this.correo = correo;
+		else
+			throw new IllegalArgumentException("El correo del profesor no es válido.");
+
+	}
+
+	public void setTelefono(String telefono) {
+		if(telefono==null) {
+			this.telefono = null;
+		} else {
+			Pattern p = Pattern.compile(ER_TELEFONO);
+			Matcher m = p.matcher(telefono);
+			if(m.matches())
+				this.telefono = telefono;
+			else
+				throw new IllegalArgumentException("El teléfono del profesor no es válido.");
+		}
+	}
+
+	public String getNombre() {
+		return nombre;
+
+	}
+
+	public String getCorreo() {
+		return correo;
+
+	}
+
+	public String getTelefono() {
+		return telefono;
+
+	}
+
+	public int hashCode() {
+		return Objects.hash(nombre, correo, telefono);
+
+	}
+
+	public boolean equals(Object o) {
+		if(o==null)
+			return false;
+		if(!(o instanceof Profesor))
+			return false;
+		Profesor otro = (Profesor) o;
+		if(this.getNombre().equals(otro.getNombre()))
+			return true;
+		return false;
+
+	}
+
+	public String toString() {
+		String devolver = "[nombre=" + getNombre() + ", correo=" + getCorreo();
+		if(getTelefono()==null)
+			devolver += "]";
+		else
+			devolver += ", telefono=" + getTelefono() + "]";
+		return devolver;
+
+	}
+}
--- src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/ProfesorTest.java
+++ src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/ProfesorTest.java
@@ -1,13 +1,19 @@
 package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 public class ProfesorTest {
-	/*
-	private static final String ERROR_EXCEPCION = "Deber√≠a haber saltado la excepci√≥n.";
-	private static final String ERROR_NO_EXCEPCION = "No deber√≠a haber saltado la excepci√≥n.";
-	private static final String nombre = "Jos√© Ram√≥n";
+	private static final String ERROR_EXCEPCION = "DeberÌa haber saltado la excepciÛn.";
+	private static final String ERROR_NO_EXCEPCION = "No deberÌa haber saltado la excepciÛn.";
+	private static final String nombre = "JosÈ RamÛn";
 	private static final String correo = "joseramon.jimenez@iesalandalus.org";
 	private static final String telefono = "950112233";
-	
+
 	Profesor profesorConTelefono = new Profesor(nombre, correo, telefono);
 	Profesor profesorSinTelefono = new Profesor(nombre, correo);
 
@@ -23,7 +29,7 @@ public class ProfesorTest {
 			fail(ERROR_NO_EXCEPCION);
 		}
 	}
-	
+
 	@Test
 	public void constructorTresParametrosValidoTest() {
 		Profesor profesor = null;
@@ -44,7 +50,7 @@ public class ProfesorTest {
 			fail(ERROR_NO_EXCEPCION);
 		}
 	}
-	
+
 	@Test
 	public void constructorDosParametrosNoValidoTest() {
 		Profesor profesor = null;
@@ -59,7 +65,7 @@ public class ProfesorTest {
 			profesor = new Profesor("", correo);
 			fail(ERROR_EXCEPCION);
 		} catch (IllegalArgumentException e) {
-			assertEquals("El nombre del profesor no puede estar vac√≠o.", e.getMessage());
+			assertEquals("El nombre del profesor no puede estar vacÌo.", e.getMessage());
 			assertNull(profesor);
 		}
 		try {
@@ -73,11 +79,11 @@ public class ProfesorTest {
 			profesor = new Profesor(nombre, "");
 			fail(ERROR_EXCEPCION);
 		} catch (IllegalArgumentException e) {
-			assertEquals("El correo del profesor no es v√°lido.", e.getMessage());
+			assertEquals("El correo del profesor no es v·lido.", e.getMessage());
 			assertNull(profesor);
 		}
 	}
-	
+
 	@Test
 	public void constructorTresParametrosNoValidoTest() {
 		Profesor profesor = null;
@@ -85,11 +91,11 @@ public class ProfesorTest {
 			profesor = new Profesor(nombre, correo, "");
 			fail(ERROR_EXCEPCION);
 		} catch (IllegalArgumentException e) {
-			assertEquals("El tel√©fono del profesor no es v√°lido.", e.getMessage());
+			assertEquals("El telÈfono del profesor no es v·lido.", e.getMessage());
 			assertNull(profesor);
 		}
 	}
-	
+
 	@Test
 	public void constructorCopiaValidoTest() {
 		Profesor profesor;
@@ -102,7 +108,7 @@ public class ProfesorTest {
 			fail(ERROR_NO_EXCEPCION);
 		}
 	}
-	
+
 	@Test
 	public void constructorCopiaNoValidoTest() {
 		Profesor profesor = null;
@@ -114,7 +120,7 @@ public class ProfesorTest {
 			assertNull(profesor);
 		}
 	}
-	
+
 	@Test
 	public void getTest() {
 		assertEquals(nombre, profesorConTelefono.getNombre());
@@ -122,31 +128,30 @@ public class ProfesorTest {
 		assertEquals(telefono, profesorConTelefono.getTelefono());
 		assertNull(profesorSinTelefono.getTelefono());
 	}
-	
+
 	@Test
 	public void equalTest() {
 		Profesor profesor = new Profesor(nombre, correo, telefono);
-		Profesor profesor1 = new Profesor("Andr√©s", correo, telefono);
+		Profesor profesor1 = new Profesor("AndrÈs", correo, telefono);
 		assertNotEquals(profesor, null);
 		assertNotEquals(profesor, "");
 		assertEquals(profesor, profesor);
 		assertEquals(profesorConTelefono, profesor);
 		assertNotEquals(profesor, profesor1);
 	}
-	
+
 	@Test
 	public void hashCodeTest() {
 		Profesor profesor = new Profesor(nombre, correo, telefono);
-		Profesor profesor1 = new Profesor("Andr√©s", correo, telefono);
+		Profesor profesor1 = new Profesor("AndrÈs", correo, telefono);
 		assertEquals(profesor.hashCode(), profesor.hashCode());
 		assertEquals(profesorConTelefono.hashCode(), profesor.hashCode());
 		assertNotEquals(profesor.hashCode(), profesor1.hashCode());
 	}
-	
+
 	@Test
 	public void toStringTest() {
-		assertEquals("[nombre=Jos√© Ram√≥n, correo=joseramon.jimenez@iesalandalus.org, telefono=950112233]", profesorConTelefono.toString());
-		assertEquals("[nombre=Jos√© Ram√≥n, correo=joseramon.jimenez@iesalandalus.org]", profesorSinTelefono.toString());
+		assertEquals("[nombre=JosÈ RamÛn, correo=joseramon.jimenez@iesalandalus.org, telefono=950112233]", profesorConTelefono.toString());
+		assertEquals("[nombre=JosÈ RamÛn, correo=joseramon.jimenez@iesalandalus.org]", profesorSinTelefono.toString());
 	}
-*/
 }
